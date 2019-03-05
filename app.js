8a443580ead77bf15f94182d18621dee671cac08
Luis Santos <lui.santos@gmail.com>
2019-03-05 09:26:15 -0500

    some clean up and actual insert statement

--- app.js
+++ app.js
@@ -2,15 +2,8 @@ const http = require('http');
 const fs = require('fs');
 const hostname = '127.0.0.1';
 const port = 3000;
-
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-        host : 'localhost',
-        database : '3rdPartyLogin',
-        user : 'root',
-        password : 'gundam',
-        insecureAuth : true
-});
+const {parse} = require('querystring');
+const mysql = require('mysql');
 
 fs.readFile('./HomePage.html', function (err,html) {
 	if (err) {
@@ -18,31 +11,63 @@ fs.readFile('./HomePage.html', function (err,html) {
 	}
 	http.createServer(function(request, response) {
 		if(request.method === 'POST') {
-			connection.connect(function(err) {
-				if(err){
-					console.error('Error connecting: ' + err.stack);
-					return;
-				}
-				console.log('Connected as id '+ connection.threadId);
-			});
-
-			connection.query('Select * FROM Google_Credentials', function (error, results, fields) {
-				if(error)
-					throw error;
-
-				results.forEach(result => {
-					console.log(result);
-				});	
-			});
-			connection.end();
-
-			response.writeHeader(200, {"Content-Type":"text/plain"});
-			response.write("done");
-			response.end();
+			postResponse(request, response);
 		} else {
-			response.writeHeader(200, {"Content-Type": "text/html"});
-			response.write(html);
-			response.end();
+			homePageDisplay(html, response);
 		}
 	}).listen(3000);
 });
+
+function postResponse(request, response){
+	var data = '';
+
+        request.on('data', chunk => {
+        	if(!(chunk === null || chunk === undefined))
+        	data += chunk.toString();
+        });
+
+        request.on('end', () => {
+        	var parsedData = parse(data);
+                writeToDB(parsedData);
+        });
+
+        response.writeHeader(200, {"Content-Type":"text/plain"});
+        response.write("Success");
+        response.end();
+}//end postResponse
+
+function writeToDB(parsedData) {
+	var connection = mysql.createConnection({
+        	host : 'localhost',
+        	database : '3rdPartyLogin',
+        	user : 'root',
+        	password : 'gundam',
+        	insecureAuth : true
+	});
+
+	connection.connect(function(err) {
+                if(err){
+	                console.error('Error connecting: ' + err.stack);
+                        return;
+                }
+                console.log('Connected as id '+ connection.threadId);
+        });
+	
+	var insertStatement = "INSERT INTO Google_Credentials (id, name, profile_image, email) ";
+	insertStatement += "VALUES (" + parsedData['id'] + ", '" + parsedData['name'] + "', '" + parsedData['picture_url'] + "', '" + parsedData['email'] + "');";
+
+        connection.query(insertStatement, function (error, results, fields) {
+        	if(error)
+                	throw error;
+               	console.log(results);
+	});
+	
+	connection.end();
+}//end writeToDB
+
+function homePageDisplay(html, response) {
+	response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+}//end homePageDisplay
+
